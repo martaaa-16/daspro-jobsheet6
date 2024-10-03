af9bb9465645d9bf4d4cd6fcf85b81fc98fb85ee
martaaa-16 <martaprama@gmail.com>
2024-10-03 12:36:53 +0700

    tugas

--- tugas.java
+++ tugas.java
@@ -2,49 +2,50 @@ import java.util.Scanner;
 
 public class tugas {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input15 = new Scanner(System.in);
 
-        String kategori_buku;
-        int jumlah_beli, totalBayar;
-        double diskon = 0, harga_buku = 20000, totaldiskon;
+        String kategori;
+        int jumlahBeli;
+        double diskon = 0, harga = 20000, totalDiskon, totalBayar;
 
+        System.out.print("Jenis Buku: ");
+        kategori = input15.nextLine();
+        System.out.print("Jumlah Buku: ");
+        jumlahBeli = input15.nextInt();
 
-        System.out.println("Jenis Buku: ");
-        kategori_buku = input.nextLine();
-        System.out.println("Jumlah Buku: ");
-        jumlah_beli = input.nextInt();
-
-        if (kategori_buku.equalsIgnoreCase("Kamus")) {
+        if (kategori.equalsIgnoreCase("Kamus")) {
             diskon = 0.1;
-            if (jumlah_beli > 2) {
+            if (jumlahBeli > 2) {
                 diskon += 0.02;
             }
-            totaldiskon = diskon * (jumlah_beli * harga_buku) ;
-            totalBayar = (int) (harga_buku *jumlah_beli - totaldiskon);
+            totalDiskon = diskon * (jumlahBeli * harga) ;
+            totalBayar = (harga * jumlahBeli) - totalDiskon;
 
-            System.out.println("diskon: " +totaldiskon);
-            System.out.println("bayar: " +totalBayar);
-        }else if (kategori_buku.equalsIgnoreCase("Novel")) {
+            System.out.println("Total Diskon: " + totalDiskon);
+            System.out.println("Total Bayar: " + totalBayar);
+        } else if (kategori.equalsIgnoreCase("Novel")) {
             diskon = 0.07;
-            if (jumlah_beli > 3) {
+            if (jumlahBeli > 3) {
                 diskon += 0.02; 
-            }else if (jumlah_beli <= 3) {
+            } else if (jumlahBeli <= 3) {
                 diskon += 0.01;
             }
-            totaldiskon = diskon * (jumlah_beli * harga_buku) ;
-            totalBayar = (int) (harga_buku *jumlah_beli - totaldiskon);
+            totalDiskon = diskon * (jumlahBeli * harga) ;
+            totalBayar = (harga * jumlahBeli) - totalDiskon;
 
-            System.out.println("diskon: " +totaldiskon);
-            System.out.println("bayar: " +totalBayar);
-        }else{
-           if (jumlah_beli > 3) {
+            System.out.println("Total Diskon: " + totalDiskon);
+            System.out.println("Total Bayar: " + totalBayar);
+       
+        } else{
+           if (jumlahBeli > 3) {
             diskon += 0.05;
-            totaldiskon = diskon * (jumlah_beli * harga_buku) ;
-            totalBayar = (int) (harga_buku *jumlah_beli - totaldiskon);
-
-            System.out.println("diskon: " +totaldiskon);
-            System.out.println("bayar: " +totalBayar);
+            
            }
+           totalDiskon = diskon * (jumlahBeli * harga) ;
+            totalBayar = (int) (harga * jumlahBeli - totalDiskon);
+
+            System.out.println("Total Diskon: " + totalDiskon);
+            System.out.println("Total Bayar: " + totalBayar);
         }
     }
 }
